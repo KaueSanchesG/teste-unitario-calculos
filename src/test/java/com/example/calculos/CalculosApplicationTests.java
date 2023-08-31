47b436174e9efc786dad1eff09f50c0c734431c8
KaueSanchesG <kauesanches123@gmail.com>
2023-08-31 18:18:49 -0300

    First commit

---
+++ src/test/java/com/example/calculos/CalculosApplicationTests.java
@@ -0,0 +1,69 @@
+package com.example.calculos;
+
+import com.example.calculos.Service.ValorService;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest
+class CalculosApplicationTests {
+
+	private ValorService valorService;
+	private List<Double> valores;
+	private double delta = 0.001;
+
+	@BeforeEach
+	void setup(){
+		valorService = new ValorService();
+		valores = Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0,
+				11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0);
+	}
+
+	@Test
+	public void testSomaDosValores(){
+		double result = 210.0;
+		double actual = valorService.calcularSoma(valores);
+		Assert.assertEquals(result, actual, delta);
+	}
+
+	@Test
+	public void testMaxValue(){
+		double result = 20.0;
+		double actual = valorService.maxValue(valores);
+		Assert.assertEquals(result, actual, delta);
+	}
+
+	@Test
+	public void testMinValue(){
+		double result = 1.0;
+		double actual = valorService.minValue(valores);
+		Assert.assertEquals(result, actual, delta);
+	}
+
+	@Test
+	public void testMedia(){
+		double result = 10.5;
+		double actual = valorService.media(valores);
+		Assert.assertEquals(result, actual, delta);
+	}
+
+/*	OBS: minha mediana está funcionando assim pois minha função retorna apenas 2 casas depois da virgula*/
+	@Test
+	public void testDesvioPadrao(){
+		double result = 5.77;
+		double actual = valorService.desvioPadrao(valores);
+		Assert.assertEquals(result, actual, delta);
+	}
+
+	@Test
+	public void testMediana(){
+		double result = 10.5;
+		double actual = valorService.mediana(valores);
+		Assert.assertEquals(result, actual, delta);
+	}
+}
+
